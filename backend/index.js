5cf8d82b360b60cbd85f56f2ca5365836ae37632
Salim31 <salimalhaddad1211@gmail.com>
2025-05-04 18:41:55 +0700

    TUTAM9 - Perbaikan URL

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const cors = require("cors"); // Import cors
 require('dotenv').config();
 
 const app = express();
-const corsOptions = {
-  origin: "http://localhost:5173", // Allow requests from your frontend
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
-};
 const port = process.env.PORT || 3000;
 
-app.use(cors(corsOptions)); // Enable CORS with the specified options
 app.use(express.json());
 
 app.use('/store', require('./src/routes/store.route'));
