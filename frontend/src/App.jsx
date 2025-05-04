070bc870187f726a3454b919179503211987c3a0
Salim31 <salimalhaddad1211@gmail.com>
2025-05-04 21:48:09 +0700

    TUTAM9 - Perbaikan URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateCard />} />
-          <Route path="/edit" element={<EditCardPage />} /> {/* Static route */}
+          <Route path="/edit" element={<EditCardPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
